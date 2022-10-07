e4cd6241f5422b5240daa1b00eae47e782a87890
Ben Reynolds <ben.reynolds@iotic-labs.com>
2022-10-07 12:02:20 +0100

    EI-1395 Add methods to send/receive input messages
    - Bumps API version to 0.0.13
    - Refactors auth example

---
+++ examples/inputs.rs
@@ -0,0 +1,154 @@
+mod auth;
+
+use serde_json::{from_str, json, Value};
+use std::{str::from_utf8, thread::sleep, time::Duration};
+
+use iotics_grpc_client::common::FeedValue;
+use iotics_grpc_client::input::{delete_input, describe_input, receive_input_messages};
+use iotics_grpc_client::interest::send_input_message;
+use iotics_grpc_client::properties::{common_keys::predicate::COMMENT, PropertyBuilder};
+use iotics_grpc_client::twin::{crud::delete_twin, upsert::upsert_twin, UpsertInputWithMeta};
+use log::{error, info, LevelFilter};
+use std::sync::Arc;
+
+use auth::{generate_twin_did, AuthBuilder, IoticsSettings};
+
+pub const INPUT_NAME: &str = "input";
+pub const VALUE_LABEL: &str = "message";
+
+#[tokio::main]
+async fn main() {
+    let log_level = std::env::var("RUST_LOG")
+        .unwrap_or_else(|_| "info".to_string())
+        .parse()
+        .unwrap_or(LevelFilter::Info);
+
+    pretty_env_logger::formatted_timed_builder()
+        .filter_level(log_level)
+        .init();
+    let settings = IoticsSettings::new();
+    let auth_builder = Arc::new(AuthBuilder::new(settings.clone()));
+
+    let sender_twin_id = generate_twin_did(&auth_builder.config, "sender");
+    let receiver_twin_id = generate_twin_did(&auth_builder.config, "receiver");
+    info!(
+        "Twin {} will be sending to an input called '{}' on twin {}",
+        sender_twin_id, INPUT_NAME, receiver_twin_id
+    );
+    info!("Creating twins...");
+    create_sender(auth_builder.clone(), &sender_twin_id).await;
+    create_receiver(auth_builder.clone(), &receiver_twin_id).await;
+    activate_receiver_thread(auth_builder.clone(), &receiver_twin_id).await;
+    send_messages(auth_builder.clone(), &sender_twin_id, &receiver_twin_id).await;
+    clean_space(auth_builder.clone(), &sender_twin_id, &receiver_twin_id).await;
+}
+
+async fn create_sender(auth_builder: Arc<AuthBuilder>, did: &str) {
+    upsert_twin(
+        auth_builder,
+        did,
+        vec![
+            PropertyBuilder::build_label("en", "sender twin"),
+            PropertyBuilder::build_lang_literal(COMMENT, "en", "rust client example script"),
+        ],
+        vec![],
+        vec![],
+        None,
+        1, //Public visibility, to be deprecated
+    )
+    .await
+    .expect("Upserting sender failed");
+}
+
+async fn create_receiver(auth_builder: Arc<AuthBuilder>, did: &str) {
+    upsert_twin(
+        auth_builder.clone(),
+        did,
+        vec![
+            PropertyBuilder::build_label("en", "receiver twin"),
+            PropertyBuilder::build_lang_literal(COMMENT, "en", "rust client example script"),
+        ],
+        vec![],
+        vec![UpsertInputWithMeta {
+            id: INPUT_NAME.to_string(),
+            // TODO: Values are for inputs too, check other libs for safety of renaming struct
+            values: vec![FeedValue {
+                label: VALUE_LABEL.to_string(),
+                comment: "a text string from the sender".to_string(),
+                unit: "".to_string(),
+                data_type: "byte".to_string(),
+            }],
+            properties: vec![],
+        }],
+        None,
+        1, // Public visibility, to be deprecated
+    )
+    .await
+    .expect("Upserting receiver failed");
+    // TODO: Make such payloads printable
+    describe_input(auth_builder, did, INPUT_NAME, None)
+        .await
+        .expect("Failed to describe input");
+}
+
+async fn activate_receiver_thread(auth_builder: Arc<AuthBuilder>, did: &str) {
+    let mut message_stream = receive_input_messages(auth_builder.clone(), did, INPUT_NAME)
+        .await
+        .expect("Activating receiver failed");
+    let fut = async move {
+        while let Some(response) = message_stream.recv().await {
+            match response {
+                Ok(message_data) => {
+                    let message_json: Value = from_str(
+                        from_utf8(&message_data).expect("UTF8 error decoding input message"),
+                    )
+                    .expect("Error deserialising message json");
+
+                    let message: &str = message_json["message"].as_str().unwrap();
+
+                    info!("MESSAGE RECEIVED: {}", message);
+                    if message == "!" {
+                        info!("Last character received!");
+                        break;
+                    }
+                }
+                Err(e) => {
+                    error!("Input stream returned error: {e:?}")
+                }
+            }
+        }
+    };
+    tokio::spawn(fut);
+}
+
+async fn send_messages(auth_builder: Arc<AuthBuilder>, sender_did: &str, receiver_did: &str) {
+    for character in "HELLO, IOTICS!".chars() {
+        let message = json!({ VALUE_LABEL: character });
+        send_input_message(
+            auth_builder.clone(),
+            None,
+            receiver_did,
+            INPUT_NAME,
+            sender_did,
+            message.to_string().into_bytes(),
+        )
+        .await
+        .unwrap_or_else(|_| panic!("Sending message {} failed", message));
+        info!("SENT MESSAGE: {}", character);
+        sleep(Duration::from_secs(1))
+    }
+}
+
+async fn clean_space(auth_builder: Arc<AuthBuilder>, sender_did: &str, receiver_did: &str) {
+    info!("Deleting twins...");
+    delete_twin(auth_builder.clone(), sender_did)
+        .await
+        .expect("Deleting sender failed");
+    // For demonstrating the method; input would be deleted anyway when twin is deleted immediately afterward.
+    delete_input(auth_builder.clone(), receiver_did, INPUT_NAME)
+        .await
+        .expect("Deleting input failed");
+    delete_twin(auth_builder.clone(), receiver_did)
+        .await
+        .expect("Deleting receiver failed");
+}
--- src/interest.rs
+++ src/interest.rs
@@ -1,17 +1,24 @@
 use anyhow::Context;
 use std::sync::Arc;
+use std::time::SystemTime;
 
-use crate::client::google::protobuf::BoolValue;
-use crate::client::iotics::api::fetch_interest_request::Arguments;
+use crate::client::google::protobuf::{BoolValue, Timestamp};
+use crate::client::iotics::api::fetch_interest_request::Arguments as FetchInterestArguments;
+use crate::client::iotics::api::input_interest::DestinationInput;
 use crate::client::iotics::api::interest::FollowedFeed;
-use crate::client::iotics::api::Feed;
-use crate::client::iotics::api::Interest;
+use crate::client::iotics::api::{Feed, Input, InputInterest, InputMessage, Interest};
 
 pub use crate::client::iotics::api::interest_api_client::InterestApiClient;
-pub use crate::client::iotics::api::{FetchInterestRequest, FetchInterestResponse};
+pub use crate::client::iotics::api::send_input_message_request::{
+    Arguments as SendMessageArguments, Payload,
+};
+
+pub use crate::client::iotics::api::{
+    FetchInterestRequest, FetchInterestResponse, SendInputMessageRequest, SendInputMessageResponse,
+};
 
 use crate::auth_builder::IntoAuthBuilder;
-use crate::common::{Channel, FeedId, Headers, HostId, Streaming, TwinId};
+use crate::common::{Channel, FeedId, Headers, HostId, InputId, Streaming, TwinId};
 use crate::helpers::generate_client_app_id;
 
 pub async fn create_interest_api_client(
@@ -65,7 +72,7 @@ pub async fn follow_with_client(
 
     let mut request = tonic::Request::new(FetchInterestRequest {
         headers: Some(headers),
-        args: Some(Arguments {
+        args: Some(FetchInterestArguments {
             interest: Some(Interest {
                 followed_feed: Some(FollowedFeed {
                     feed: Some(Feed {
@@ -103,4 +110,103 @@ pub async fn follow_with_client(
         .into_inner();
 
     Ok(stream)
+}
+
+pub async fn send_input_message<T: Into<Vec<u8>>>(
+    auth_builder: Arc<impl IntoAuthBuilder>,
+    receiver_host_id: Option<&str>,
+    receiver_twin_id: &str,
+    input_id: &str,
+    sender_twin_id: &str,
+    data: T,
+) -> Result<(), anyhow::Error> {
+    let mut client = create_interest_api_client(auth_builder.clone()).await?;
+
+    send_input_message_with_client(
+        auth_builder,
+        &mut client,
+        receiver_host_id,
+        receiver_twin_id,
+        input_id,
+        sender_twin_id,
+        data,
+    )
+    .await
+}
+
+#[allow(clippy::too_many_arguments)]
+pub async fn send_input_message_with_client<T: Into<Vec<u8>>>(
+    auth_builder: Arc<impl IntoAuthBuilder>,
+    client: &mut InterestApiClient<Channel>,
+    receiver_host_id: Option<&str>,
+    receiver_twin_id: &str,
+    input_id: &str,
+    sender_twin_id: &str,
+    data: T,
+) -> Result<(), anyhow::Error> {
+    let client_app_id = generate_client_app_id();
+    let transaction_ref = vec![client_app_id.clone()];
+
+    let headers = Headers {
+        client_app_id,
+        transaction_ref: transaction_ref.clone(),
+        ..Default::default()
+    };
+    let host_id = receiver_host_id.map(|id| HostId {
+        value: id.to_string(),
+    });
+
+    let args = SendMessageArguments {
+        interest: Some(InputInterest {
+            dest_input: Some(DestinationInput {
+                host_id,
+                input: Some(Input {
+                    id: Some(InputId {
+                        value: input_id.to_string(),
+                    }),
+                    twin_id: Some(TwinId {
+                        value: receiver_twin_id.to_string(),
+                    }),
+                }),
+            }),
+            sender_twin_id: Some(TwinId {
+                value: sender_twin_id.to_string(),
+            }),
+        }),
+    };
+
+    let dtm = std::time::SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
+    let timestamp = Timestamp {
+        seconds: dtm.as_secs() as i64,
+        nanos: 0,
+    };
+
+    let payload = Payload {
+        message: Some(InputMessage {
+            occurred_at: Some(timestamp),
+            mime: "application/json".to_string(),
+            data: data.into(),
+        }),
+    };
+
+    let mut request = tonic::Request::new(SendInputMessageRequest {
+        headers: Some(headers.clone()),
+        args: Some(args.clone()),
+        payload: Some(payload.clone()),
+    });
+
+    let token = auth_builder.get_token()?;
+
+    request.metadata_mut().append(
+        "authorization",
+        token.parse().context("parse token failed")?,
+    );
+    if let Err(e) = client.send_input_message(request).await {
+        return Err(anyhow::Error::new(e).context(format!(
+            "Sending message failed, transaction ref [{}]",
+            transaction_ref.join(", ")
+        )));
+    }
+
+    Ok(())
 }
